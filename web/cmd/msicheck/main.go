d9030fb4743342a66a1593eee398770639b61d0c
Takekazu Omi <takekazuomi@gmail.com>
2024-02-14 11:21:58 +0900

    ko build and cleanup

--- web/cmd/msicheck/main.go
+++ web/cmd/msicheck/main.go
@@ -19,7 +19,7 @@ import (
 
 var (
 	help bool   = false
-	host string = ""
+	host string = "0.0.0.0"
 	port int    = 5000
 
 	storageAccountName string
@@ -28,14 +28,12 @@ var (
 const EnvPrefix string = "MSI_"
 
 func init() {
-	fmt.Println("init()")
 	flag.BoolVarP(&help, "help", "h", false, "show this help message")
 	flag.StringVarP(&storageAccountName, "storage-account-name", "n", "", "storage account name")
-	flag.StringVar(&host, "host", "", "listening host name")
-	flag.IntVarP(&port, "port", "p", 5000, "listening port")
+	flag.StringVar(&host, "host", host, "listening host name")
+	flag.IntVarP(&port, "port", "p", port, "listening port")
 	flag.Parse()
 	ApplyEnv(EnvPrefix)
-	fmt.Println("end init()")
 }
 
 func ApplyEnv(prefix string) []error {
@@ -72,15 +70,20 @@ func GetCredential() (*azidentity.DefaultAzureCredential, error) {
 
 func ListContainer(ctx context.Context, cred *azidentity.DefaultAzureCredential) ([]string, error) {
 
-	serviceClient, err := azblob.NewServiceClient(fmt.Sprintf("https://%v.blob.core.windows.net", storageAccountName), cred, nil)
+	//	serviceClient, err := azblob.NewServiceClient(fmt.Sprintf("https://%v.blob.core.windows.net", storageAccountName), cred, nil)
+	client, err := azblob.NewClient(fmt.Sprintf("https://%v.blob.core.windows.net", storageAccountName), cred, nil)
 	if err != nil {
 		return nil, err
 	}
-	pager := serviceClient.ListContainers(nil)
+
+	pager := client.NewListContainersPager(nil)
 
 	ret := []string{}
-	for pager.NextPage(ctx) {
-		resp := pager.PageResponse()
+	for pager.More() {
+		resp, err := pager.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
 
 		for _, v := range resp.ContainerItems {
 			ret = append(ret, *v.Name)
@@ -115,6 +118,12 @@ func main() {
 			http.Error(w, m, http.StatusInternalServerError)
 		}
 		c, err := ListContainer(r.Context(), cred)
+		if err != nil {
+			m := fmt.Sprintf("ListContainer error: %v", err)
+			log.Println(m)
+			http.Error(w, m, http.StatusInternalServerError)
+		}
+
 		for _, v := range c {
 			w.Write([]byte(v))
 			w.Write([]byte("\n"))
@@ -122,7 +131,8 @@ func main() {
 	})
 
 	addr := fmt.Sprintf("%v:%v", host, port)
-	log.Printf("Listen %v\n", addr)
+	log.Printf("Start Listen %v\n", addr)
+	log.Printf("http://%v/msicheck\n", addr)
 	setAzLogging()
 
 	err := http.ListenAndServe(addr, handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
--- web/go.mod
+++ web/go.mod
@@ -15,7 +15,6 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.2 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
